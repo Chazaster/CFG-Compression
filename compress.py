3f987979414ea8b0ed0a8364b14ff504c7274fd3
Chase Watson <chasetwatson@gmail.com>
2018-04-22 16:45:35 -0700

    Frequency queue working

--- compress.py
+++ compress.py
@@ -1,45 +1,74 @@
 # Chase Watson, Adam May, Matt Mulkeen
-import queue as queue
+''''
+Questions to ask Ravi:
+    1. Are the replace functions for sequitur and repair the same
+    or does repair have a different replace function than sequitur?
 
+    2. How do you implement the Linked List, Hash Table, and Priority
+    Queue for the Repair replace function as described in the slides?
+
+    3. What is the runtime for sequitur?
+
+    4. How would you Zero-order compress (Huffman Encoding)
+     the string after finding all pairs for Re-pair?
+'''
+
+import queue
+
+# Function to create pairs from the inputted string
 def createPairs(string):
     pairArray = []
     for i in range (len(string) - 1):
         pairArray.append(string[i] + string[i + 1])
     return pairArray
 
+# Function that finds the number of times each pair occurs in the pairArray
 def pairFrequency(pairs):
+    # tempArray will append a 1 each time a similar pair is seen,
+    # and will store a 0 if the pair hasn't been seen yet.
+    # This array will get reset each iteration through the loop
     tempArray = []
-    q = queue.Queue
-    for i in range (len(pairs)):
+    # Queue will contain the frequencies and its indicies will correspond to the pairArray indicies
+    # This will be linked later in another function
+    q = queue.Queue()
+    pairsSize = len(pairs)
+    for i in range (pairsSize):
+        temp = pairs[i]
+        # While looping through pairArray, save current pair in temp
+        # and loop through pairArray again to compare temp with other
+        # elements in the pairArray.
+        for j in range(pairsSize):
+            count = 0
+            # If we see a similar pair, count = 1 and append it to temp array
+            if temp == pairs[j]:
+                count += 1
+            # Otherwise append a 0 (meaning we didnt see the same pair)
+            tempArray.append(count)
+
+        # Now we add up the total number of times we saw the pair
+        # by just totalling all the 1's in tempArray
         size = len(tempArray)
+        # Make sure tempArray is populated, if it isn't don't do anything
         if size != 0:
             total = 0
             for k in range(size):
                 total += tempArray[k]
             q.put(total)
             del tempArray[:]
-
-        temp = pairs[i]
-        for j in range (len(pairs)):
-            count = 0
-            if temp == pairs[j]:
-                count += 1
-            tempArray.append(count)
     return q
 
 def sequitur(input):
     return input
 
-# Very recursive
-def repair(input):
+def repair(pairs, queue):
     return input
 
 def main():
     string = input("Enter a single string of any length using lowercase characters in the language {a - z}: ")
     seq = sequitur(string)
-    re = repair(string)
     print("After Sequitur: " + seq)
-    print("After Re-Pair: " + re)
     pairs = createPairs(string)
-    print(pairFrequency(pairs))
+    queue = pairFrequency(pairs)
+    #re = repair(pairs, queue)
+    #print("After Re-Pair: " + re)
 main()
